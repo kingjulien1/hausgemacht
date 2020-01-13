19d479f39b0d1730352f802f1e43457fb192a6f0
julian-blaschke <julianblaschke@icloud.com>
2020-01-13 19:34:29 +0100

    adding first page

---
+++ hausgemacht-web-app/src/pages/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { PageHeader } from "antd";
+
+export default function Landing() {
+  return (
+    <PageHeader
+      title="Hausgemacht"
+      subTitle="ein Platz für Familienrezepte"
+    ></PageHeader>
+  );
+}
